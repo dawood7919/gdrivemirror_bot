eb9056a596daf395a4ee28b22dd39705d7d79e53
dawood7919 <61195728+dawood7919@users.noreply.github.com>
2020-04-15 05:48:04 +0200

    Update and rename credentials.py.example to credentials.py

---
+++ modules/credentials.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 Atul-Kadian
+
+class Creds(object):
+        TOKEN = "1286446214:AAExOEmrpqKx2W6VVBz9LET6snuvuoeOqeQ"
+        CLIENT_ID = "500431112185-rva75p0jaost9uidg25mlkb9cnq1hpuo.apps.googleusercontent.com"
+        CLIENT_SECRET = "0y59Wqx_xeTcyAgY5renl-hD"
+
